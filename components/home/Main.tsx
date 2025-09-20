792a1009fc1c2c58ac37a1fa514718a41e8198ec
lrq <1632108322@qq.com>
2025-09-20 22:02:15 +0800

    just divide the district and add a button

---
+++ components/home/Main.tsx
@@ -0,0 +1,3 @@
+export default function Main(){
+  return <main className="flex-1 text-gray-900  bg-white  dark:text-gray-100 dark:bg-gray-800">主题</main>
+}
---
+++ components/home/Navigation.tsx
@@ -0,0 +1,6 @@
+import Button from "@/components/common/Button"
+export default function Navigation(){
+  return <nav className="bg-gray-900 w-[260px] h-full text-gray-300 p-2">
+    <Button>新建对话</Button>
+  </nav>
+}
