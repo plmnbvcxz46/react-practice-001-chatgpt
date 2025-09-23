af2555d862c13dd28070fbb267b520ccf9390994
lrq <1632108322@qq.com>
2025-09-23 21:42:28 +0800

    完成夜间模式了，tailwind cs 4.1更改了实现逻辑，要注意global css里的文件，官方文档里有相关解释

--- components/home/Navigation/Toolbar.tsx
+++ components/home/Navigation/Toolbar.tsx
@@ -1,8 +1,9 @@
 import Button from "@/components/common/Button"
 import { MdDarkMode, MdInfo, MdLightMode } from "react-icons/md"
+import { LuPanelLeft } from "react-icons/lu"
 import { useAppContext } from "@/components/AppContext"
 
-export default function Toolbar(){
+export default function Navigation(){
   const { state:{theme}, setState } = useAppContext()
   return (
     <div className="absolute bottom-0 left-0 right-0 bg-gray-800 flex p-2 justify-between">
